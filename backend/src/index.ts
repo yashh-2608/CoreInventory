17abdc910e6d5b51e303ccf9aac93741290707e6
yashh-2608 <yashradhanpura24@gmail.com>
2026-04-15 19:35:37 +0530

    fix: render port issue

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -55,5 +55,5 @@ app.use((error: unknown, req: express.Request, res: express.Response, next: expr
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
